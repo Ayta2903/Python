a338c401f7ad18da3c56d51d2f136f057f1cc22d
Ayta2903 <aytaivanova8662@gmail.com>
2025-12-09 18:48:49 +0900

    lession1

--- lesson1/lession_1_task_3.py
+++ lesson1/lession_1_task_3.py
@@ -1,3 +1,3 @@
-first_name = input("Айталина")
-last_name = input("Иванова")
-print(f"Вас зовут:{last_name}{first_name}")
+first_name = input("Ваше имя")
+last_name = input("Ваше фамилия")
+print(f"Вас зовут:{last_name} {first_name}")
